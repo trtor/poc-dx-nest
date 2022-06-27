3bdf5887b7132ba936a95ba802629707de63f179
sirawathlab <sirawat.yam@hlabconsulting.com>
2022-06-27 20:22:47 +0700

    fix port

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import dotenv from dotenv;
-
-// dotenv.config({ path: '.env' });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(5000);
+  await app.listen(process.env.NODE_PORT || 5000);
 }
 bootstrap();
